9c190c7316c6657454aa1957afd0607b3c211f62
Veronika Shportko <Veronika_Shportko@epam.com>
2018-04-27 10:42:30 +0300

    task1: typescript added, unused views removed

---
+++ SocialNetwork/appScriptsJS/app.js
@@ -0,0 +1,8 @@
+function Welcome(person) {
+    return "<h2>Hello " + person + ", Lets learn TypeScript</h2>";
+}
+function ClickMeButton() {
+    var user = "MithunVP";
+    document.getElementById("divMsg").innerHTML = Welcome(user);
+}
+//# sourceMappingURL=app.js.map
